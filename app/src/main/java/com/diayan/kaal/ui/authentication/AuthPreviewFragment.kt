f4b133f90e0e75ba55591d8fda33cee193b4ed93
diayan <diayans@outlook.com>
2020-08-31 14:28:55 +0000

    remove toolbar for certain from auth preview

---
+++ app/src/main/java/com/diayan/kaal/ui/authentication/AuthPreviewFragment.kt
@@ -0,0 +1,43 @@
+package com.diayan.kaal.ui.authentication
+
+import android.content.Intent
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.diayan.kaal.R
+
+class AuthPreviewFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AuthPreviewFragment()
+    }
+
+    private lateinit var viewModel: AuthPreviewViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.auth_preview_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(AuthPreviewViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity).supportActionBar?.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity).supportActionBar?.show()
+    }
+}
--- app/src/main/java/com/diayan/kaal/ui/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/diayan/kaal/ui/authentication/AuthenticationActivity.kt
@@ -8,5 +8,7 @@ class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
+
+        actionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
