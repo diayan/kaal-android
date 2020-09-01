0b10bf89f2e9d672116b812a1d2ab9f5c0e929d8
diayan <diayans@outlook.com>
2020-09-01 11:56:30 +0000

    setup navigation between fragments using the back button

--- app/src/main/java/com/diayan/kaal/ui/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/diayan/kaal/ui/authentication/AuthenticationActivity.kt
@@ -2,13 +2,31 @@ package com.diayan.kaal.ui.authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.diayan.kaal.R
 
 class AuthenticationActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
 
         actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navController = this.findNavController(R.id.auth_nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.auth_nav_host_fragment)
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+
     }
 }
