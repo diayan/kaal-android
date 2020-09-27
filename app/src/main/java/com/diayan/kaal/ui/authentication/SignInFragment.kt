eb27a8631c4457faca429f00cf5881df8ac8b8cc
diayan <diayans@outlook.com>
2020-09-27 08:20:12 +0000

    sign in implementation [WIP]

--- app/src/main/java/com/diayan/kaal/ui/authentication/SignInFragment.kt
+++ app/src/main/java/com/diayan/kaal/ui/authentication/SignInFragment.kt
@@ -1,22 +1,39 @@
 package com.diayan.kaal.ui.authentication
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.diayan.kaal.MainActivity
 import com.diayan.kaal.databinding.SignInFragmentBinding
 import com.diayan.kaal.di.Injectable
 import com.diayan.kaal.di.injectViewModel
+import com.diayan.kaal.helper.SharedPrefManager
+import com.diayan.kaal.util.IntentUtil
+import com.diayan.kaal.util.Utils
 import javax.inject.Inject
 
 class SignInFragment : Fragment(), Injectable {
 
     @set:Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
+
     private lateinit var viewModel: AuthViewModel
+
+    private val sharedPreferencesManager: SharedPrefManager by lazy {
+        sharedPreferencesManager
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +41,77 @@ class SignInFragment : Fragment(), Injectable {
     ): View? {
 
         viewModel = injectViewModel(viewModelFactory)
-
         val binding = SignInFragmentBinding.inflate(inflater)
+
+
+        viewModel.formState.observe(viewLifecycleOwner, Observer {
+            val formState = it ?: return@Observer
+
+            // disable login button unless both username / password is valid
+            binding.signInButton.isEnabled = formState.isDataValid
+
+            if (formState.emailError != null) {
+                binding.emailEditText.error = getString(formState.emailError)
+            }
+
+            if (formState.passwordError != null) {
+                binding.passwordEditText.error = getString(formState.passwordError)
+            }
+
+            viewModel.formResult.observe(viewLifecycleOwner, Observer {
+                Log.e("FormResult", it.toString())
+                val formResult = it ?: return@Observer
+                binding.loader.visibility = View.GONE
+                if (formResult.error != null) {
+                    showLoginFailure(formResult.error)
+                }
+                if (formResult.success != null) {
+                    openNextActivity()
+                }
+            })
+
+            binding.emailEditText.doAfterTextChanged {
+                viewModel.loginDataChanged(
+                    binding.emailEditText.text.toString(),
+                    binding.passwordEditText.text.toString()
+                )
+            }
+
+            binding.emailEditText.apply {
+
+                doAfterTextChanged {
+                    viewModel.loginDataChanged(
+                        binding.emailEditText.text.toString(),
+                        binding.passwordEditText.text.toString()
+                    )
+                }
+
+                setOnEditorActionListener { textView, i, keyEvent ->
+                    when (i) {
+                        EditorInfo.IME_ACTION_DONE ->
+                            viewModel.loginWith(
+                                binding.emailEditText.text.toString(),
+                                binding.passwordEditText.text.toString()
+                            )
+                    }
+                    false
+                }
+
+                binding.signInButton.setOnClickListener {
+                    binding.loader.visibility = View.VISIBLE
+                    Utils.hideKeyboard(activity as MainActivity)
+
+                    viewModel.loginWith(
+                        binding.emailEditText.text.toString(),
+                        binding.passwordEditText.text.toString()
+                    )
+                }
+            }
+
+            //forgot password button comes here
+            //forgot_password_textView.setOnClickListener { launchActivity<ForgotPasswordActivity> {} }
+        })
+
         return binding.root
     }
 
@@ -33,5 +119,15 @@ class SignInFragment : Fragment(), Injectable {
         super.onResume()
         (activity as AppCompatActivity).supportActionBar?.title = "Sign In"
         (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun openNextActivity() {
+        IntentUtil.start(context, MainActivity::class.java)
+        activity?.setResult(Activity.RESULT_OK)
+        activity?.finish()
+    }
+
+    private fun showLoginFailure(error: String) {
+        Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
     }
 }
